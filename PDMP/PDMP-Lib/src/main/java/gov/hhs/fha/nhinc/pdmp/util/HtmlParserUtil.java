315885ecd15dcc262fdbb4c16165217012968d40
Minh-Hai Nguyen <minh-hai.nguyen@cgi.com>
2018-11-21 15:59:04 -0500

    update based on comments

--- PDMP/PDMP-Lib/src/main/java/gov/hhs/fha/nhinc/pdmp/util/HtmlParserUtil.java
+++ PDMP/PDMP-Lib/src/main/java/gov/hhs/fha/nhinc/pdmp/util/HtmlParserUtil.java
@@ -52,8 +52,17 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class HtmlParserUtil {
-
     private static final Logger logger = LoggerFactory.getLogger(HtmlParserUtil.class);
+    private static final String FILL_AT = "filled_at";
+    private static final String DRUG = "drug";
+    private static final String PRODUCT_NAME = "product_name";
+    private static final String QUANTITY = "quantity";
+    private static final String DAYS_SUPPLY = "days_supply";
+    private static final String SOURCE_STATE_CD = "source_state_code";
+    private static final String PRESCRIPTIONS = "prescriptions";
+    private static final String RX = "rx";
+    private static final String SCRIPT_TAG = "script";
+    private static final String JS_REPORT_METHOD = "window.DataReportPageSetup()(";
 
     /**
      * Retrieves patient prescription report from the given URL and adds extracts the prescriptions from the report HTML
@@ -81,12 +90,12 @@ public class HtmlParserUtil {
         throws MalformedURLException, IOException {
         File file = new File(fileName);
         logger.debug("File Location {} ", file.toPath());
-        Document htmlDocument = Jsoup.parse(new File(fileName), "UTF-8");
+        Document htmlDocument = Jsoup.parse(file, "UTF-8");
         return populatePrescriptionList(htmlDocument);
     }
 
     private static List<PrescriptionInfo> populatePrescriptionList(Document htmlDocument) {
-        Elements elements = htmlDocument.select("script");
+        Elements elements = htmlDocument.select(SCRIPT_TAG);
         Element data = elements.get(1);
         List<Node> childNodes = data.childNodes();
         Node node = childNodes.get(0);
@@ -95,10 +104,10 @@ public class HtmlParserUtil {
             DataNode childDataNode = (DataNode) node;
             String content = childDataNode.getWholeData();
             content = StringUtils.trim(content);
-            content = StringUtils.removeStart(content, "window.DataReportPageSetup()(");
+            content = StringUtils.removeStart(content, JS_REPORT_METHOD);
             content = StringUtils.removeEnd(content, ");");
             JSONObject jsonObject = new JSONObject(content);
-            JSONArray prescriptions = jsonObject.getJSONObject("rx").getJSONArray("prescriptions");
+            JSONArray prescriptions = jsonObject.getJSONObject(RX).getJSONArray(PRESCRIPTIONS);
             prescriptions.forEach(prescription -> {
                 JSONObject obj = (JSONObject) prescription;
                 prescriptionList.add(populatePrescriptionInfo(obj));
@@ -109,11 +118,11 @@ public class HtmlParserUtil {
 
     private static PrescriptionInfo populatePrescriptionInfo(JSONObject row) {
         PrescriptionInfo prescriptionInfo = new PrescriptionInfo();
-        prescriptionInfo.setFileStrDate(row.getString("filled_at"));
-        prescriptionInfo.setDrugName(row.getJSONObject("drug").getString("product_name"));
-        prescriptionInfo.setDrugCount(row.getJSONObject("drug").getInt("quantity"));
-        prescriptionInfo.setDrugDuration(row.getInt("days_supply"));
-        prescriptionInfo.setPmpState(row.getString("source_state_code"));
+        prescriptionInfo.setFileStrDate(row.getString(FILL_AT));
+        prescriptionInfo.setDrugName(row.getJSONObject(DRUG).getString(PRODUCT_NAME));
+        prescriptionInfo.setDrugCount(row.getJSONObject(DRUG).getInt(QUANTITY));
+        prescriptionInfo.setDrugDuration(row.getInt(DAYS_SUPPLY));
+        prescriptionInfo.setPmpState(row.getString(SOURCE_STATE_CD));
         return prescriptionInfo;
     }
 }
